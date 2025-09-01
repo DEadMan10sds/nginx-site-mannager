4a2bc610505f86178caf6a6762f7b45efe8c1483
DEadMan10sds <alejandroadan.sds@gmail.com>
2025-09-01 12:22:41 -0600

    ADD: List files function IMPROVED: Code

--- nginx-site-mannager.c
+++ nginx-site-mannager.c
@@ -1,32 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>   // chdir
-#include <limits.h>   // PATH_MAX
+#include <unistd.h>
+#include <limits.h>
 #include <errno.h>
+#include <dirent.h>
 
 #define MAX_PATH_LEN 4096
 
+#define NGINX_DEFAULT_PATH "/etc/nginx/sites-enabled" 
+
 void change_directory(char *path);
+void list_files(char *path);
 
 int main(void) {
-    
-    change_directory("/root");
-    system("ls -la");
+    list_files(NGINX_DEFAULT_PATH);
 
     return 0;
 }
 
 
 void change_directory(char *path){
-    char destination_directory[MAX_PATH_LEN + 4];  // "cd " + espacio + path + '\0'
+    char destination_directory[MAX_PATH_LEN + 4]; 
     int n = snprintf(destination_directory, sizeof(destination_directory), "cd %s", path);
+    
     if (n < 0 || (size_t)n >= sizeof(destination_directory)) {
-        fprintf(stderr, "Ruta demasiado larga\n");
+        fprintf(stderr, "Route too long\n");
     }
-
 
     if (chdir(path) != 0) {
-        fprintf(stderr, "chdir('%s') falló: %s\n", path, strerror(errno)); 
+        fprintf(stderr, "chdir('%s') failed: %s\n", path, strerror(errno)); 
     }
+}
+
+void list_files(char *path){
+    int counter = 0;
+    struct dirent *current_directory;
+    DIR *directories = opendir(path);
+
+    if(!directories)
+       printf("Couldn't open the directory");
+    
+    while ((current_directory = readdir(directories)))
+    {
+        if(!strcmp(current_directory->d_name, ".") || !strcmp(current_directory->d_name,"..")) 
+            continue;
+
+        counter++;
+        printf("%i.-  %s\n", counter, current_directory->d_name);
+    }
+    
+    closedir(directories);
 }
